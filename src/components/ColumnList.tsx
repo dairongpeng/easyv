49c307c7dc11dd7d977e553446fc292c6ecad165
dai <darope@163.com>
2022-05-29 01:47:28 +0800

    react事件

--- src/components/ColumnList.tsx
+++ src/components/ColumnList.tsx
@@ -25,14 +25,35 @@ interface ColumnProps {
     description: string;
 }
 
-class ColumnList extends React.Component<ColumnProps> {
+interface State {
+    count: number;
+}
+
+class ColumnList extends React.Component<ColumnProps, State> {
     constructor(props: ColumnProps) {
         super(props);
+        this.state = {
+            count: 1
+        };
+        // ts报错：无法分配"count",因为它是只读属性
+        // this.state.count = 2;
+
+        // 强行绑定，否在在编译阶段找不到handlerClick
+        this.handlerClick = this.handlerClick.bind(this);
+    }
+
+    uniques() {
+        this.setState({count: 3});
+    }
+
+    handlerClick(event: any) {
+        this.setState({count: 2});
+        console.log(this.state.count);
     }
 
     render() {
-        return (
-        <li>
+        return ( // jsx
+        <li onClick={this.handlerClick}>
             <img alt="logo" src={this.props.url}/>
             <h2>{this.props.title}</h2>
             <p>{this.props.description}</p>
