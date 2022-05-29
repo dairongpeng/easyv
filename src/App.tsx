633dc65aa4a6504a46c998ad8c01aff32abe815d
dai <darope@163.com>
2022-05-30 01:45:18 +0800

    react hook及副作用函数

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,27 @@
-import React from 'react';
+// 使用{useState}，用来定义组件内部需要使用到的state
+import React,{useState, useEffect} from 'react';
+import logo from "./logo.svg";
 import ColumnList from './components/ColumnList'
 import dataList from './mock/ColumnList.json'
 import styles from './App.module.css';
 // 使其导入axios依赖
 import axios from "axios";
 
-interface Props{}
-
-// 定义当前的State
-interface State {
-  dataSource: any[];
-  // 用来标识State中的数据是否被加载完成了
-  havedata: boolean;
-}
-
-// 改造App函数为App类
-class App extends React.Component<Props, State> {
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      dataSource:[],
-      havedata: false,
-    };
-  }
-
-  componentDidMount() {
-    // 在componentDidMount生命周期中使用axios请求数据，放到State里面
-    axios.get("https://jsonplaceholder.typicode.com/users").then((data) => {
-        // [{...},{...}....]
-        // console.log(data.data);
-        this.setState({
-          dataSource: data.data,
-          havedata: true,
-        });
-      });
-  }
-
-  render() {
-    return (
-      <div>
-      <ul className = {styles.list}>
-        {dataList.map((l) => 
-        <ColumnList url={l.url} description={l.description} title={l.title}/>)}
-      </ul>
-      {/* 显示我们上文通过axios请求过来的数据 */}
-      <ul>
-          {/* 如果数据没被加载，显示为loding...，加载成功了解析出来email进行显示 */}
-          {!this.state.havedata
-          ? "loding..." 
-          : this.state.dataSource.map((u) => <li>{u.email}</li>)
-          }
-      </ul>
-      </div>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0); // 这里是一个hook
+  // 当我们点击下文click按钮后，会调用setCount方法，修改p标签，重新渲染。
+  // 修改渲染完p后，会调用useEffect方法。找到当前网页title标题，动态渲染标题
+  // 由于useEffect是在组件内部声明的，所以该方法可以访问到App里面的props和state
+  useEffect(() => {
+    // 通过ES6的${}来植入变量
+    document.title = `you clicked ${count} times`
+  });
+  return (
+    <div>
+      <p>you clicked {count} times</p>
+      <button onClick={() => {setCount(count + 1)}}>click me</button>
+    </div>
+  )
 }
 
 export default App;
